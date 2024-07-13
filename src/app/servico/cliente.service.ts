3cc1f22c68a4c4712403169c64a7bab9f5d817ed
Gabriel Mendonca <gabriel-peres@hotmail.com>
2024-07-13 10:19:55 -0300

    Formulario angular

---
+++ src/app/servico/cliente.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {Observable  } from 'rxjs';
+import { Cliente } from '../modelo/Cliente';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+ // URL da API
+
+ private url:string = 'http://localhost:8080';
+  constructor(private http:HttpClient) { }
+
+  // Método para selecionar todos os clientes
+  selecionar():Observable<Cliente[]>{
+    return this.http.get<Cliente[]>(this.url);
+  }
+}
